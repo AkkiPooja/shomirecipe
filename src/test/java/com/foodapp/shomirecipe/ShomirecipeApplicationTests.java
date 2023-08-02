a10662312a977d51b88244902398dc7ec95d3be7
AkkiPooja <poojaakki111@gmail.com>
2023-08-01 23:35:35 -0400

    Initial Setup

---
+++ src/test/java/com/foodapp/shomirecipe/ShomirecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodapp.shomirecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShomirecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
